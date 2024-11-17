7ee07b033eaf5943f65e91df82c337ba7eb57d3b
Aseema thasneem <aseemathasneem@gmail.com>
2024-11-17 21:15:14 +0530

    env variable not loading

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+console.log(process.env.CORS_ORIGIN)
 // Enable CORS
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
